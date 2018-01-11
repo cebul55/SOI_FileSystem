e9277c49dd30900b2f9022bfcbf8ae8dde2132ad
B.Cybulski <B.Cybulski@stud.elka.pw.edu.pl>
2018-01-12 00:55:13 +0100

    first2 commit

---
+++ cmake-build-debug/CMakeFiles/SOI_FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SOI_FileSystem.dir/main.cpp.o"
+  "SOI_FileSystem.pdb"
+  "SOI_FileSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SOI_FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
